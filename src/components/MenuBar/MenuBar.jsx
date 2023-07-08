a30672198d6863fcb85f3125d71ca85a7b0a8982
Kristi <kristiana.d.ingco@gmail.com>
2023-07-07 20:14:08 -0700

    Add interactive font dropdown

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import { ReactComponent as Logo } from "../../assets/images/logo.svg";
 import { Checkbox, Menu, Dropdown } from "semantic-ui-react";
 
 const MenuBar = () => {
+  const [currentFont, setCurrentFont] = useState("Sans Serif");
   return (
     <Menu fluid secondary>
       <Logo />
       {/* Font Menu */}
       <Menu.Item position='right'>
-        <Dropdown />
+        <Dropdown text={currentFont}>
+          <Dropdown.Menu>
+            <Dropdown.Item
+              text='Sans Serif'
+              active={currentFont === "Sans Serif"}
+              onClick={() => setCurrentFont("Sans Serif")}
+            />
+            <Dropdown.Item
+              text='Serif'
+              active={currentFont === "Serif"}
+              onClick={() => setCurrentFont("Serif")}
+            />
+            <Dropdown.Item
+              text='Mono'
+              active={currentFont === "Mono"}
+              onClick={() => setCurrentFont("Mono")}
+            />
+          </Dropdown.Menu>
+        </Dropdown>
         <Checkbox toggle />
       </Menu.Item>
 
