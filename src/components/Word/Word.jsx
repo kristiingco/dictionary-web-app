95126b0052e10737a9662a28f20131ddff200f4a
Kristi <kristiana.d.ingco@gmail.com>
2023-07-09 18:46:28 -0700

    Add handling if no definitions exist

--- src/components/Word/Word.jsx
+++ src/components/Word/Word.jsx
@@ -10,7 +10,7 @@ import {
 import { WordContext } from "../../contexts/WordContext";
 
 const Word = () => {
-  const { wordInfo, setSearchWord } = useContext(WordContext);
+  const { wordInfo, wordExists, setSearchWord } = useContext(WordContext);
   const { word, phonetic, phonetics, meanings, sourceUrls } = wordInfo;
 
   let audio = null;
@@ -26,57 +26,66 @@ const Word = () => {
   };
   return (
     <Container fluid>
-      <Grid columns={2} fluid>
-        <Grid.Row stretched>
-          <Grid.Column>
-            <Segment basic>
-              <Header as='h1'>{word}</Header>
-            </Segment>
+      {wordExists ? (
+        <div>
+          <Grid columns={2} fluid>
+            <Grid.Row stretched>
+              <Grid.Column>
+                <Segment basic>
+                  <Header as='h1'>{word}</Header>
+                </Segment>
 
-            <Segment basic>{phonetic && <span>{phonetic}</span>}</Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment textAlign='right' basic>
-              <Button
-                circular
-                icon={{ name: "play", color: "purple" }}
-                size='massive'
-                onClick={onPlay}
-              />
-            </Segment>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-      {meanings &&
-        meanings.map(({ partOfSpeech, definitions, synonyms }, idx) => {
-          return (
-            <div key={idx}>
-              <Divider horizontal>{partOfSpeech}</Divider>
-              <span>Meaning</span>
-              <ul>
-                {definitions &&
-                  definitions.map(({ definition }) => {
-                    return <li>{definition}</li>;
-                  })}
-              </ul>
-              {synonyms && synonyms.length > 0 && (
-                <div>
-                  Synonyms:
-                  {synonyms.map((synonym, idx) => {
-                    return (
-                      <span onClick={() => setSearchWord(synonym)}>
-                        {synonym}
-                        {idx !== synonyms.length - 1 ? "," : ""}
-                      </span>
-                    );
-                  })}
+                <Segment basic>{phonetic && <span>{phonetic}</span>}</Segment>
+              </Grid.Column>
+              <Grid.Column>
+                <Segment textAlign='right' basic>
+                  <Button
+                    circular
+                    icon={{ name: "play", color: "purple" }}
+                    size='massive'
+                    onClick={onPlay}
+                  />
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+          {meanings &&
+            meanings.map(({ partOfSpeech, definitions, synonyms }, idx) => {
+              return (
+                <div key={idx}>
+                  <Divider horizontal>{partOfSpeech}</Divider>
+                  <span>Meaning</span>
+                  <ul>
+                    {definitions &&
+                      definitions.map(({ definition }) => {
+                        return <li>{definition}</li>;
+                      })}
+                  </ul>
+                  {synonyms && synonyms.length > 0 && (
+                    <div>
+                      Synonyms:
+                      {synonyms.map((synonym, idx) => {
+                        return (
+                          <span onClick={() => setSearchWord(synonym)}>
+                            {synonym}
+                            {idx !== synonyms.length - 1 ? "," : ""}
+                          </span>
+                        );
+                      })}
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-          );
-        })}
-      <Divider />
-      {sourceUrls && <span>Source: {sourceUrls[0]}</span>}
+              );
+            })}
+          <Divider />
+          {sourceUrls && <span>Source: {sourceUrls[0]}</span>}
+        </div>
+      ) : (
+        <div>
+          <Header as='h2'>{wordInfo.title}</Header>
+          <span>{wordInfo.resolution}</span>
+        </div>
+      )}
     </Container>
   );
 };
--- src/contexts/WordContext.jsx
+++ src/contexts/WordContext.jsx
@@ -5,30 +5,40 @@ export const WordContext = createContext({
   setSearchWord: () => null,
   wordInfo: {},
   setWordInfo: () => null,
+  wordExists: false,
 });
 
 export const WordProvider = ({ children }) => {
   const [searchWord, setSearchWord] = useState("hello");
   const [wordInfo, setWordInfo] = useState({});
+  const [wordExists, setWordExists] = useState(true);
 
-  useEffect(() => {
-    const searchWordChangeHandler = () => {
-      fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`)
-        .then((response) => response.json())
-        .then((data) => {
+  const searchWordChangeHandler = () => {
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.resolution) {
+          setWordExists(false);
+          setWordInfo(data);
+        } else {
+          setWordExists(true);
           setWordInfo(data[0]);
           console.log(data[0]);
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
-    };
+        }
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
     searchWordChangeHandler();
+    console.log(wordExists);
   }, [searchWord]);
 
   return (
     <WordContext.Provider
-      value={{ searchWord, setSearchWord, wordInfo, setWordInfo }}
+      value={{ searchWord, setSearchWord, wordInfo, setWordInfo, wordExists }}
     >
       {children}
     </WordContext.Provider>
