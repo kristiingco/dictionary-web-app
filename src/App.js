42cfb71ad206235831f2ed8807f7eeaadc56f700
Kristi <kristiana.d.ingco@gmail.com>
2023-07-21 17:43:10 -0700

    Start implementing dark mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useContext } from "react";
+
 import { Container } from "semantic-ui-react";
 import Dictionary from "./components/Dictionary/Dictionary";
 import MenuBar from "./components/MenuBar/MenuBar";
 
+import { DarkModeContext } from "./contexts/DarkModeContext";
+
 import "./App.css";
 
 function App() {
+  const { isDarkMode } = useContext(DarkModeContext);
   return (
-    <div className='App'>
+    <div className={`App ${isDarkMode ? "dark-mode" : ""}`}>
       <Container className='container--main'>
         <MenuBar />
         <Dictionary />
